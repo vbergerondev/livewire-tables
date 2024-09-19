24fcb98b99a648efb1852f9915a32e6a516b9805
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2024-09-18 20:08:09 -0400

    WIP

--- resources/views/components/toolbar/columns.blade.php
+++ resources/views/components/toolbar/columns.blade.php
@@ -5,29 +5,31 @@
     </svg>
 </button>
 
-<div wire:ignore.self x-data="{ selectedColumns: @entangle('selectedColumns') }" class="modal modal-lg fade" id="exampleModalCols" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Columns</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <ul class="list-group">
-                    @foreach($this->tableColumns as $c)
-                        <li class="list-group-item">
-                            <input class="form-check-input me-1" type="checkbox" x-model="selectedColumns" value="{{ $c->field }}" id="{{ $c->field }}">
-                            <label class="form-check-label stretched-link" for="{{ $c->field }}">
-                                {{ $c->name }}
-                            </label>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" x-ref="close" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" @click="$wire.void(); $refs.close.click()">Save changes</button>
+@teleport('body')
+    <div wire:ignore.self x-data="{ selectedColumns: @entangle('selectedColumns') }" class="modal modal-lg fade" id="exampleModalCols" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Columns</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <ul class="list-group">
+                        @foreach($this->tableColumns as $c)
+                            <li class="list-group-item">
+                                <input class="form-check-input me-1" type="checkbox" x-model="selectedColumns" value="{{ $c->field }}" id="{{ $c->field }}">
+                                <label class="form-check-label stretched-link" for="{{ $c->field }}">
+                                    {{ $c->name }}
+                                </label>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" x-ref="close" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" @click="$wire.void(); $refs.close.click()">Save changes</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endteleport
